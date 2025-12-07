0e15a90d4fd293f8726103162d145d141bf25356
mickeyl11l <mikitenkodima11@gmail.com>
2025-12-07 12:24:36 +0200

    Update ConfettiButton.tsx

--- components/ConfettiButton.tsx
+++ components/ConfettiButton.tsx
@@ -1,24 +1,14 @@
 'use client'
-
 import { Button } from '@/components/ui/button'
-import { forwardRef } from 'react'
 import confetti from 'canvas-confetti'
 
-const ConfettiButton = forwardRef<HTMLButtonElement, React.ButtonHTMLAttributes<HTMLButtonElement>>(
-  (props, ref) => {
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-      confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: { y: 0.6 },
-      })
-      if (props.onClick) props.onClick(e)
-    }
-
-    return <Button {...props} ref={ref} onClick={handleClick} />
-  }
-)
-
-ConfettiButton.displayName = 'ConfettiButton'
-
-export { ConfettiButton }
+export function ConfettiButton({ children, className = "" }: { children: React.ReactNode, className?: string }) {
+  return (
+    <Button
+      className={className}
+      onClick={() => confetti({ particleCount: 150, spread: 70, origin: { y: 0.6 } })}
+    >
+      {children}
+    </Button>
+  )
+}
