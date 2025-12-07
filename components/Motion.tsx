c75157cd1d568b1d656927c7306642ddf664c8b5
mickeyl11l <mikitenkodima11@gmail.com>
2025-12-07 12:11:39 +0200

    Create Motion.tsx

---
+++ components/Motion.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { motion } from 'framer-motion'
+import { HTMLMotionProps } from 'framer-motion'
+
+export const MotionDiv = motion.div
+export const MotionSection = motion.section
+export const MotionH1 = motion.h1
+export const MotionP = motion.p
+export const MotionButton = motion.button
